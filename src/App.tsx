def7a2979ec45a41fa56cec109d07d90933a0e02
dLevz <100078678+danlevison@users.noreply.github.com>
2024-01-08 19:00:47 +0000

    style: Add grid section styles with grid layout, full width, padding, overflow and add display flex to main element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss"
 import Sidebar from "./components/Sidebar"
+import Grid from "./components/Grid"
 
 export function App() {
 	return (
 		<main className="app">
 			<Sidebar />
+			<Grid />
 		</main>
 	)
 }
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,3 +1,8 @@
 export default function Grid() {
-	return <section data-testid="grid"></section>
+	return (
+		<section
+			className="grid"
+			data-testid="grid"
+		></section>
+	)
 }
